2b0a4f9deb42b745fe51df070d37b26630365123
Ean244 <ianlim224@gmail.com>
2022-08-15 22:05:05 +0800

    bug fix

--- centros-server/src/msal/auth-provider.js
+++ centros-server/src/msal/auth-provider.js
@@ -153,7 +153,7 @@ class AuthProvider {
      * @param {Object} res: express response object
      */
     handleRedirect = async (req, res) => {
-
+        console.log("in handleRedirect")
         const state = JSON.parse(this.cryptoProvider.base64Decode(req.query.state));
 
         // check if nonce matches
@@ -208,29 +208,29 @@ class AuthProvider {
                     break;
                 }
 
-                case constants.AppStages.ACQUIRE_TOKEN: {
-                    // get the name of the resource associated with scope
-                    const resourceName = "graphAPI"
-
-                    const tokenRequest = {
-                        code: req.query.code,
-                        scopes: this.appSettings.resources[resourceName].scopes, // scopes for resourceName
-                        redirectUri: this.appSettings.settings.redirectUri,
-                    };
-
-                    try {
-                        const tokenResponse = await this.msalClient.acquireTokenByCode(tokenRequest);
-                        console.log("\nResponse: \n:", tokenResponse);
-
-                        req.session[resourceName].accessToken = tokenResponse.accessToken;
-                        return res.status(200).redirect(state.path);
-
-                    } catch (error) {
-                        console.log(error);
-                        res.status(500).send(error);
-                    }
-                    break;
-                }
+                // case constants.AppStages.ACQUIRE_TOKEN: {
+                //     // get the name of the resource associated with scope
+                //     const resourceName = "graphAPI"
+                //
+                //     const tokenRequest = {
+                //         code: req.query.code,
+                //         scopes: this.appSettings.resources[resourceName].scopes, // scopes for resourceName
+                //         redirectUri: this.appSettings.settings.redirectUri,
+                //     };
+                //
+                //     try {
+                //         const tokenResponse = await this.msalClient.acquireTokenByCode(tokenRequest);
+                //         console.log("\nResponse: \n:", tokenResponse);
+                //
+                //         req.session[resourceName].accessToken = tokenResponse.accessToken;
+                //         return res.status(200).redirect(state.path);
+                //
+                //     } catch (error) {
+                //         console.log(error);
+                //         res.status(500).send(error);
+                //     }
+                //     break;
+                // }
 
                 default:
                     res.status(500).send(ErrorMessages.CANNOT_DETERMINE_APP_STAGE);
@@ -248,78 +248,78 @@ class AuthProvider {
      * @param {Object} res: express response object
      * @param {Function} next: express next
      */
-    getToken = async (req, res, next) => {
-
-        // get scopes for token request
-        const scopes = this.appSettings.resources.graphAPI.scopes;
-        const resourceName = "graphAPI"
-
-        if (!req.session[resourceName]) {
-            req.session[resourceName] = {
-                accessToken: null,
-                resourceResponse: null,
-            };
-        }
-
-        try {
-
-            let account;
-
-            try {
-                account = await this.msalClient.getTokenCache().getAccountByHomeId(req.session.homeAccountId);
-
-                if (!account) {
-                    throw new Error(ErrorMessages.INTERACTION_REQUIRED);
-                }
-
-            } catch (error) {
-                console.log(error);
-                throw new InteractionRequiredAuthError(ErrorMessages.INTERACTION_REQUIRED);
-            }
-
-            const silentRequest = {
-                account: account,
-                scopes: scopes,
-            };
-
-            // acquire token silently to be used in resource call
-            const tokenResponse = await this.msalClient.acquireTokenSilent(silentRequest)
-            // console.log("\nSuccessful silent token acquisition:\n Response: \n:", tokenResponse);
-
-            // In B2C scenarios, sometimes an access token is returned empty.
-            // In that case, we will acquire token interactively instead.
-            if (tokenResponse.accessToken.length === 0) {
-                console.log(ErrorMessages.TOKEN_NOT_FOUND);
-                throw new InteractionRequiredAuthError(ErrorMessages.INTERACTION_REQUIRED);
-            }
-
-            req.session[resourceName].accessToken = tokenResponse.accessToken;
-            return next();
-
-        } catch (error) {
-            // in case there are no cached tokens, initiate an interactive call
-            if (error.name === 'InteractionRequiredAuthError') {
-
-                const state = this.cryptoProvider.base64Encode(
-                    JSON.stringify({
-                        stage: constants.AppStages.ACQUIRE_TOKEN,
-                        path: req.route.path,
-                        nonce: req.session.nonce
-                    })
-                );
-
-                // initiate the first leg of auth code grant to get token
-                this.getAuthCode(
-                    this.msalConfig.auth.authority,
-                    scopes,
-                    state,
-                    this.appSettings.settings.redirectUri,
-                    req,
-                    res
-                );
-            }
-        }
-    }
+    // getToken = async (req, res, next) => {
+    //
+    //     // get scopes for token request
+    //     const scopes = this.appSettings.resources.graphAPI.scopes;
+    //     const resourceName = "graphAPI"
+    //
+    //     if (!req.session[resourceName]) {
+    //         req.session[resourceName] = {
+    //             accessToken: null,
+    //             resourceResponse: null,
+    //         };
+    //     }
+    //
+    //     try {
+    //
+    //         let account;
+    //
+    //         try {
+    //             account = await this.msalClient.getTokenCache().getAccountByHomeId(req.session.homeAccountId);
+    //
+    //             if (!account) {
+    //                 throw new Error(ErrorMessages.INTERACTION_REQUIRED);
+    //             }
+    //
+    //         } catch (error) {
+    //             console.log(error);
+    //             throw new InteractionRequiredAuthError(ErrorMessages.INTERACTION_REQUIRED);
+    //         }
+    //
+    //         const silentRequest = {
+    //             account: account,
+    //             scopes: scopes,
+    //         };
+    //
+    //         // acquire token silently to be used in resource call
+    //         const tokenResponse = await this.msalClient.acquireTokenSilent(silentRequest)
+    //         // console.log("\nSuccessful silent token acquisition:\n Response: \n:", tokenResponse);
+    //
+    //         // In B2C scenarios, sometimes an access token is returned empty.
+    //         // In that case, we will acquire token interactively instead.
+    //         if (tokenResponse.accessToken.length === 0) {
+    //             console.log(ErrorMessages.TOKEN_NOT_FOUND);
+    //             throw new InteractionRequiredAuthError(ErrorMessages.INTERACTION_REQUIRED);
+    //         }
+    //
+    //         req.session[resourceName].accessToken = tokenResponse.accessToken;
+    //         return next();
+    //
+    //     } catch (error) {
+    //         // in case there are no cached tokens, initiate an interactive call
+    //         if (error.name === 'InteractionRequiredAuthError') {
+    //
+    //             const state = this.cryptoProvider.base64Encode(
+    //                 JSON.stringify({
+    //                     stage: constants.AppStages.ACQUIRE_TOKEN,
+    //                     path: req.route.path,
+    //                     nonce: req.session.nonce
+    //                 })
+    //             );
+    //
+    //             // initiate the first leg of auth code grant to get token
+    //             this.getAuthCode(
+    //                 this.msalConfig.auth.authority,
+    //                 scopes,
+    //                 state,
+    //                 this.appSettings.settings.redirectUri,
+    //                 req,
+    //                 res
+    //             );
+    //         }
+    //     }
+    // }
 
     // ============== GUARD ===============
 
