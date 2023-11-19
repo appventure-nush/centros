6c33f45a2d7578c17fe97cf9d99096f61b9b637d
Oon Han <hanxyn888@gmail.com>
2023-11-19 20:08:48 +0800

    removed public descriptions and venues hopefully 2

--- centros-client/src/views/CalendarView.vue
+++ centros-client/src/views/CalendarView.vue
@@ -522,19 +522,7 @@
                 <v-list-item-subtitle>{{ selectedEvent.email }}</v-list-item-subtitle>
               </v-list-item-content>
             </v-list-item>
-            <v-list-item>
-              <v-list-item-icon>
-                <v-icon>mdi-text</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>{{ selectedEvent.description }}</v-list-item-content>
-            </v-list-item>
-
-            <v-list-item v-if="selectedEvent.venue">
-              <v-list-item-icon>
-                <v-icon>mdi-map-marker</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>{{ selectedEvent.venue }}</v-list-item-content>
-            </v-list-item>
+            
             <v-list-item two-line>
               <v-list-item-icon>
                 <v-icon>mdi-calendar-clock</v-icon>
